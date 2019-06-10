2277212ad0a2024d6239d78db9b2ae0203f70cbd
unknown <835127236@qq.com>
2019-06-10 22:18:27 +0800

    upload img

--- server/pytorchServer/views.py
+++ server/pytorchServer/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def predict_index(request):
+    if request.method == 'POST':
+        print(request.FILES.get('img'))
+        print(1)
+    return render(request, 'predict.html')
