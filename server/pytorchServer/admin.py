d8261ec1934a6942fd2f5228d50e7de400516a91
roronoaws <1355386037@qq.com>
2019-06-15 12:41:42 +0800

    uploadImg

--- server/pytorchServer/admin.py
+++ server/pytorchServer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import predict_index, Post
 
 # Register your models here.
+
+admin.site.register(predict_index)
+admin.site.register(Post)
--- server/pytorchServer/urls.py
+++ server/pytorchServer/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.predict_index, name='predict_index')
+    path('predict/', views.predict_index, name='predict_index'),
+    path('post/', views.Post.as_view()),
+    path('show/', views.show)
 ]
